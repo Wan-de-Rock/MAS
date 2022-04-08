f7382a4f8534be91ab8a2e5d3ddca17d8a2a7348
Wan-de-Rock <serverDiDi20110@gmail.com>
2022-04-08 17:08:17 +0200

    Add first project ver 1.0

---
+++ MAS/MP1/Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP1
+{
+    [Serializable]
+    public class Item : IComparable<Item>
+    {
+        
+        //public int ID { get; }
+        public int Size { get; }
+        public string? Name { get; }
+        public ItemType Type { get; }
+
+
+        //private static int counter = Storage.GetNumberOfAllItems();
+
+        
+        public Item(int size, string? name, ItemType type)
+        {
+            //ID = ++counter;
+            Size = size;
+            Name = name;
+            Type = type;
+        }
+
+        public int CompareTo(Item? other)
+        {
+            if (Size > other.Size)
+                return 1;
+            if (Size < other.Size)
+                return -1;
+            return 0;
+        }
+
+        public override string? ToString()
+        {
+            return //$"ID: {ID}\t" +
+                $"Size: {Size}\t" +
+                $"Name: {Name}\t" +
+                $"Type: {Type}";
+        }
+    }
+
+    public enum ItemType
+    {
+        A,
+        B,
+        C
+    }
+}
---
+++ MAS/MP1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using MP1;
+
+
+var store1 = new Storage();
+var store2 = new Storage();
+
+Console.WriteLine(Storage.GetNumberOfAllItems());
+
+var a1 = new Item(5, "a1", ItemType.A);
+var b1 = new Item(5, null, ItemType.B);
+var c1 = new Item(4, null, ItemType.C);
+var c2 = new Item(3, "c2", ItemType.C);
+var c3 = new Item(2, "c3", ItemType.C);
+var c4 = new Item(1, "c4", ItemType.C);
+var c5 = new Item(5, null, ItemType.C);
+
+
+
+store1.AddItem(a1);
+store1.AddItem(b1);
+store1.AddItem(c1);
+store1.AddItem(c2);
+store1.AddItem(c3);
+
+store2.AddItem(c4);
+store2.AddItem(c5);
+
+
+foreach (var item in store1.StoreHouse)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\n\n");
+
+foreach (var item in store2.StoreHouse)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine(Storage.GetItemsCountForType(ItemType.A));
+Console.WriteLine(Storage.GetItemsCountForType(ItemType.B));
+Console.WriteLine(Storage.GetItemsCountForType(ItemType.C));
