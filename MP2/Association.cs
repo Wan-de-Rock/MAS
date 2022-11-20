5fbc05c6eee15dd414eaddaae37a427ceb6fd550
Wan-de-Rock <serverdidi20110@gmail.com>
2022-11-20 06:51:59 +0100

    mp2 small fixes

--- MP2/Association.cs
+++ MP2/Association.cs
@@ -1,121 +1,115 @@
-﻿
-namespace MP2
+﻿namespace MP2;
+
+public class Engine { }
+public class Person
 {
-    public class Engine { }
-    public class CarType { }
-    public class Person
+    public string Name { get; }
+    public Station ?Station { get; set; }
+    public List<Rent> Rents { get; private set; } = new List<Rent>();
+
+    public Person(string name)
     {
-        public string Name { get; }
-        public List<Rent> Rents { get; private set; }
-
-        public Person(string name)
-        {
-            Name = name;
-            Rents = new List<Rent>();
-        }
-
-        public void AddRent(Rent rent)
-        {
-            if (Rents.Contains(rent))
-                return;
-
-            Rents.Add(rent);
-        }
-        public void RemoveRent(Rent rent)
-        {
-            if (Rents.Contains(rent))
-                Rents.Remove(rent);
-        }
-    }
-    public class Car
-    {
-        public int Id { get; }
-        public int DayRentalCost { get; }
-        public CarType Type { get; set; } // Zwykła
-        public Engine Engine { get; } // Kompozycja
-        public Station Station { get; set; }
-        public List<Rent> Rents { get; private set; }
-
-        private static int id = 0;
-        public Car(int dayRentalCost)
-        {
-            Id = ++id;
-            Engine = new Engine();
-            DayRentalCost = dayRentalCost;
-            Rents = new List<Rent>();
-        }
-
-        public void AddRent(Rent rent)
-        {
-            if (Rents.Contains(rent))
-                return;
-
-            Rents.Add(rent);
-        }
-
-        public void RemoveRent(Rent rent)
-        {
-            if (Rents.Contains(rent))
-                Rents.Remove(rent);
-        }
-
-        public override string ToString()
-        {
-            return $"ID: {Id} | Rental: {DayRentalCost} | Station: {Station.Id}";
-        }
+        Name = name;
     }
 
-    public class Rent
+    public void AddRent(Car car, int time)
     {
-        public int Time { get; }
-        public int Price => Car.DayRentalCost * Time;
+        var rent = new Rent(time, car, this);
+        if (Rents.Contains(rent))
+            return;
 
-        private Car Car;
-        private Person Person;
-
-        public Rent(int time, Car car, Person person)
-        {
-            Time = time;
-            Car = car;
-            Person = person;
-        }
-        public override string ToString()
-        {
-            return $"Person: {Person.Name}\nCar Identificator: {Car.Id}\nTotal Price: {Price}";
-        }
+        Rents.Add(rent);
+        car.AddRent(rent);
+    }
+    public void RemoveRent(Rent rent)
+    {
+        if (Rents.Contains(rent))
+            Rents.Remove(rent);
     }
 
-    public class Station
+    public override string ToString()
     {
-        public int Id { get; }
+        return $"Name: {Name}\nStation:\n{Station}";
+    }
+}
+public class Car
+{
+    public int Id { get; }
+    public int DayRentalCost { get; }
+    public Engine Engine { get; }
+    public List<Rent> Rents { get; private set; } = new List<Rent>();
 
-        public Dictionary<int, Car> Cars { get; private set; } = new Dictionary<int, Car>();
-
-        private static int id = 0;
-        public Station()
-        {
-            Id = ++id;
-        }
-
-        public void AddCar(Car car)
-        {
-            if (!Cars.ContainsKey(car.Id))
-                Cars.Add(car.Id, car);
-        }
-        public void RemoveCar(int id)
-        {
-            if (Cars.ContainsKey(Id))
-                Cars.Remove(id);
-        }
-        public Car GetCar(int id)
-        {
-            return Cars[id];
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, Cars);
-        }
+    private static int counter = 0;
+    public Car(int dayRentalCost)
+    {
+        Id = counter++;
+        Engine = new Engine();
+        DayRentalCost = dayRentalCost;
     }
 
+    public void AddRent(Rent rent)
+    {
+        if (Rents.Contains(rent))
+            return;
+
+        Rents.Add(rent);
+    }
+
+    public void RemoveRent(Rent rent)
+    {
+        if (Rents.Contains(rent))
+            Rents.Remove(rent);
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id} | Rental: {DayRentalCost}";
+    }
+}
+
+public class Rent
+{
+    public Car Car { get; }
+    public Person Person { get; }
+    public int Time { get; }
+    public int Price => Car.DayRentalCost * Time;
+
+    public Rent(int time, Car car, Person person)
+    {
+        Time = time;
+        Car = car;
+        Person = person;
+    }
+    public override string ToString()
+    {
+        return $"Person: {Person.Name}\nCar Identificator: {Car.Id}\nTotal Price: {Price}";
+    }
+}
+
+public class Station
+{
+    public Person ?Owner { get; set; }
+    public Dictionary<int, Car> Cars { get; private set; } = new Dictionary<int, Car>();
+
+    public void AddCar(Car car)
+    {
+        if (Cars.ContainsKey(car.Id))
+            return;
+
+        Cars.Add(car.Id, car);
+    }
+    public void RemoveCar(int id)
+    {
+        if (Cars.ContainsKey(id))
+            Cars.Remove(id);
+    }
+    public Car GetCar(int id)
+    {
+        return Cars[id];
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, Cars);
+    }
 }
--- MP2/Program.cs
+++ MP2/Program.cs
@@ -1,31 +1,66 @@
 ﻿using MP2;
 
-var type1 = new CarType();
-var type2 = new CarType();
-
-var car1 = new Car(100);
-var car2 = new Car(200);
-var car3 = new Car(300);
+var car1 = new Car(110);
+var car2 = new Car(270);
+var car3 = new Car(350);
+var car4 = new Car(250);
+var car5 = new Car(400);
+var car6 = new Car(120);
+var car7 = new Car(480);
 
 var person1 = new Person("Nick");
 var person2 = new Person("Jack");
 var person3 = new Person("Tom");
 
-car1.Type = type1;
+var station1 = new Station();
+var station2 = new Station();
+var station3 = new Station();
 
-var rent = new Rent(10, car3, person3);
-car3.AddRent(rent);
-person3.AddRent(rent);
+station1.AddCar(car1);
+station1.AddCar(car2);
+station1.AddCar(car3);
 
-Console.WriteLine(rent + "\n");
+station2.AddCar(car4);
+station2.AddCar(car5);
 
-var station = new Station();
-station.AddCar(car1);
-station.AddCar(car2);
-station.AddCar(car3);
+station3.AddCar(car6);
+station3.AddCar(car7);
 
-car1.Station = station;
-car2.Station = station;
-car3.Station = station;
+person1.AddRent(car3, 7);
+person1.AddRent(car6, 9);
+person2.AddRent(car2, 5);
+person2.AddRent(car7, 3);
+person3.AddRent(car1, 2);
+person3.AddRent(car4, 4);
+person3.AddRent(car5, 8);
 
-Console.WriteLine(station);
+person1.Station = station1;
+station1.Owner = person1;
+person2.Station = station2;
+station2.Owner = person2;
+person3.Station = station3;
+station3.Owner = person3;
+
+Console.WriteLine(person1 + "\n");
+Console.WriteLine(person2 + "\n");
+Console.WriteLine(person3 + "\n");
+
+foreach (var rent in person1.Rents)
+{
+    Console.WriteLine(rent);
+}
+Console.WriteLine();
+foreach (var rent in person2.Rents)
+{
+    Console.WriteLine(rent);
+}
+Console.WriteLine();
+foreach (var rent in person3.Rents)
+{
+    Console.WriteLine(rent);
+}
+
+
+//Console.WriteLine(station1);
+//Console.WriteLine(station2);
+//Console.WriteLine(station3);
