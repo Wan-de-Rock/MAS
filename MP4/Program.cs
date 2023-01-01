743055bbaa262673f4e290fa51e35ccb0685d999
Denis Diedov <80519500+Wan-de-Rock@users.noreply.github.com>
2023-01-01 20:16:48 +0100

    Mini project 4 init

---
+++ MP4/Program.cs
@@ -0,0 +1,27 @@
+﻿using MP4;
+
+var person1 = new Person("Misha", "00320512995");
+Person person2 = null;
+try
+{
+    person2 = new Person("Sasha", "0032051299a");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+person1.Pesel = "03301512921"; // Attribute
+person2 = new Person("Sasha", "00320512995");
+try
+{
+    person2.Pesel = "03301512921"; // Unique
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);	
+}
+
+
+Console.WriteLine(person1);
+Console.WriteLine(person2);
---
+++ MP4/Restrictions.cs
@@ -0,0 +1,71 @@
+﻿namespace MP4;
+public class Person
+{
+    private const int NUMBER_OF_DIGITS_IN_PESEL = 11;
+
+    private string _pesel;
+
+    public string Name { get; private set; }
+
+    public string Pesel
+    {
+        get { return _pesel; }
+        set
+        {
+            if (ValidatePesel(value))
+            {
+                if (peselDB.ContainsKey(value))
+                {
+                    if (peselDB[value] == this)
+                        return;
+                    else
+                        throw new ArgumentException("Pesel is not unique");                    
+                }
+
+                if (_pesel != null)
+                {
+                    if (peselDB.ContainsKey(_pesel))
+                        peselDB.Remove(_pesel);
+                }
+
+                _pesel = value;
+                peselDB.Add(_pesel, this);
+            }
+        }
+    }
+
+    private static Dictionary<string, Person> peselDB = new Dictionary<string, Person>();
+
+    public Person(string name, string pesel)
+    {
+        Name = name;
+        Pesel = pesel;
+    }
+
+    private bool ValidatePesel(string pesel)
+    {
+        if (pesel.Length != NUMBER_OF_DIGITS_IN_PESEL)
+            throw new ArgumentException("Number of digits in PESEL number is not correct");
+
+        var peselNumbers = new byte[NUMBER_OF_DIGITS_IN_PESEL];
+        for (int i = 0; i < NUMBER_OF_DIGITS_IN_PESEL; i++)
+        {
+            try
+            {
+                peselNumbers[i] = byte.Parse(pesel[i].ToString());
+            }
+            catch (FormatException)
+            {
+                throw new FormatException($"Character on {i} position is not integer");
+            }
+        }
+
+
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}\tPesel: {Pesel}";
+    }
+}
