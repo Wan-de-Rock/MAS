717da55fd9e24f561545146e42a2245ba65b48a2
Denis Diedov <80519500+Wan-de-Rock@users.noreply.github.com>
2023-01-01 20:16:33 +0100

    Mini project 5 init

---
+++ MP5/Entities.cs
@@ -0,0 +1,69 @@
+﻿namespace MP5;
+
+using System.Collections.Generic;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+
+    public Person(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}\tSurname: {Surname}";
+    }
+}
+
+public class Driver
+{
+    public int Id { get; set; }
+    public Person Person { get; set; }
+    public HashSet<Car> Cars { get; set; } = new HashSet<Car>();
+    public Driver(Person person)
+    {
+        Person = person;
+    }
+
+    public override string ToString()
+    {
+        return $"Driver {Person} drives cars {Cars.Count}";
+    }
+}
+
+public class Mechanic
+{
+    public int Id { get; set; }
+    public Person Person { get; set; }
+    public HashSet<Car> Cars { get; set; } = new HashSet<Car>();
+
+    public Mechanic(Person person)
+    {
+        Person = person;
+    }
+
+    public override string ToString()
+    {
+        return $"Mechanic {Person} repairs cars {Cars.Count}";
+    }
+}
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public HashSet<Driver> Drivers { get; set; }
+    public HashSet<Mechanic> Mechanics { get; set; }
+
+    public Car()
+    {
+        Drivers = new HashSet<Driver>();
+        Mechanics = new HashSet<Mechanic>();
+    }
+}
